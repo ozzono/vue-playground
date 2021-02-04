04a8f7adecd67f729d7a4a923d45d732cbab6d96
Hugo Virgilio <huvirgilio@gmail.com>
2021-02-03 22:02:54 -0300

    random commit: "I fell asleep reading a dull book, and I dreamt that I was reading on, so I woke up from sheer boredom."

---
+++ src/components/TextNButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card flat class="d-inline-flex align-center justify-center" :width="`12ch`">
+        <v-card flat rounded class="d-flex">texto</v-card>
+        <v-card flat width="2em">
+            <v-btn plain>
+                <v-icon>mdi-minus-box</v-icon>
+            </v-btn>
+        </v-card>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
